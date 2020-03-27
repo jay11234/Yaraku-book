24ac732235a96f7878d49a88eb9dc314429e99be
Eden Park <2015015407@student.sit.ac.nz>
2020-03-28 11:36:27 +1300

    Author and Book model created and coded according to the database design

---
+++ src/app/Book.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Book extends Model
+{
+    protected $table = 'books';
+    protected $fillable = ['title', 'published_year', 'genre'];
+    protected $hidden = ['created_at', 'updated_at'];
+}
