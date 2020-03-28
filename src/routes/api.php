48493867af71dc0031d295c87609f409cd4f62c7
Eden Park <2015015407@student.sit.ac.nz>
2020-03-28 14:18:35 +1300

    Basic routes for api and web are added.

--- src/routes/api.php
+++ src/routes/api.php
@@ -1,7 +1,11 @@
 <?php
 
+namespace App\Http\Controllers\v1;
+
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
+use function Ramsey\Uuid\v1;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +18,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::group(['prefix' => 'v1'], function () {
+    Route::resource('/books', BookController::class)->except(['create', 'edit']);
+    Route::resource('/authors', AuthorController::class)->except(['create', 'edit']);
 });
