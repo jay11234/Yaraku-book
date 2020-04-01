0509d64f96cc205ef7746d54049ea50a3d3bea0c
Eden Park <2015015407@student.sit.ac.nz>
2020-04-01 21:06:12 +1300

    The first page, which can be a base for SPA added and coded appropriately.

---
+++ src/resources/views/app.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="{{ app()->getLocale() }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+ 
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title>Yaraku Book STore</title>
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+
+<body>
+    <div id="app"></div>
+
+    <script src="{{ asset('js/app.js') }}"></script>
+</body>
+
+</html>
