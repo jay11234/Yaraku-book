df4ee62344914a9e72e25772e48397092033a268
Eden Park <2015015407@student.sit.ac.nz>
2020-04-04 13:49:15 +1300

    Changed Laravel to use Reactjs and app.js file added so Laravel can use ReactJs

--- src/resources/js/app.js
+++ src/resources/js/app.js
@@ -5,6 +5,7 @@
  */
 
 require('./bootstrap');
+require('./components/App');
 
 /**
  * Next, we will create a fresh React component instance and attach it to
@@ -12,4 +13,4 @@ require('./bootstrap');
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
 
-require('./components/Example');
+ 
